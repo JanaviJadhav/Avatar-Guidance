c5c24e15190f2e6574beaf73e804d831da5cd886
JanaviJadhav <117957984+JanaviJadhav@users.noreply.github.com>
2024-07-04 14:27:39 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
 import gradio as gr
 from avatar import demo
-
-app = FastAPI()
-
+app =FastAPI
 @app.get('/')
 async def root():
-    return {'message': 'Gradio app is running at /gradio'}
+    return 'Gradio app is ruuning at /gradio',200
+app=gr.mount_gradio_app(app ,demo,path='/gradio')
 
-app = gr.mount_gradio_app(app, demo, path='/gradio')
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)  # Make sure this line binds to 0.0.0.0
 
